f2a944333a0479b448e175e99d0a02dc1ffce399
Zacaria CHTATAR <zc@42.am>
2016-06-15 00:47:25 +0200

    [ #21 ][ FIX ] : gulp copies db into dist folder

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,14 +4,14 @@ var gulp = require('gulp');
 var filesToWatch = ['src/**/*.js', 'gulp/**/*.js', 'gulpfile.js',
   'package.json', '!**/node_modules/**'];
 
-gulp.task('watch-retaliation', ['babel'], function() {
+gulp.task('watch-retaliation', ['copy-db'], function() {
   gulp.watch(filesToWatch, ['serve-retaliation']);
 });
 
-gulp.task('watch-cluster', ['babel'], function() {
+gulp.task('watch-cluster', ['copy-db'], function() {
   gulp.watch(filesToWatch, ['serve-cluster']);
 });
 
-gulp.task('watch', ['babel'], function() {
+gulp.task('watch', ['copy-db'], function() {
   gulp.watch(filesToWatch, ['serve']);
 });
