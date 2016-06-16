8a1e0d3e93db72d8a818413128db8919106c8ec1
jfaverie <jean.faverie@gmail.com>
2016-06-16 14:34:30 +0200

    [ #44 ] [ REFACT ] clean module.exports expression

--- src/core/controllers/launcher.js
+++ src/core/controllers/launcher.js
@@ -2,25 +2,28 @@ import Launcher from '../models/launcher';
 
 module.exports = (dao) => {
 
+  function getLaunchers(req, res) => {
+    res.json(dao.getLaunchers());
+  }
+
+   function getLauncher(req, res) => {
+     // FIXME getLauncher not implemented in rainstorm/persistence/launcher.js
+    res.json(dao.getLauncher(req.params.id));
+  }
+
+  function updateLauncher(req, res) => {
+    let launcher = new Launcher(req.body.name, req.body.rainstormId, req.body.positions);
+    dao.updateLauncher(launcher, req.params.id, function(err) {
+      if (err) {
+        throw err;
+      }
+      res.json({message: 'Launcher updated.', data: launcher});
+    });
+  }
   return {
-    getLaunchers:(req, res) => {
-      res.json(dao.getLaunchers());
-    },
-
-    getLauncher: (req, res) => {
-      // FIXME getLauncher not implemented in rainstorm/persistence/launcher.js
-      res.json(dao.getLauncher(req.params.id));
-    },
-
-    updateLauncher: (req, res) => {
-      let launcher = new Launcher(req.body.name, req.body.rainstormId, req.body.positions);
-      dao.updateLauncher(launcher, req.params.id, function(err) {
-        if (err) {
-          throw err;
-        }
-        res.json({message: 'Launcher updated.', data: launcher});
-      });
-    }
+    getLaunchers,
+    getLauncher,
+    updateLauncher
   };
 
 };
--- src/core/controllers/user.js
+++ src/core/controllers/user.js
@@ -2,31 +2,37 @@ import User from '../models/user';
 
 module.exports = (dao) => {
 
+  function getUsers (req, res) => {
+    res.json(dao.getUsers());
+  }
+
+  function getUser(req, res) => {
+    res.json(dao.getUser(req.params.name));
+  }
+
+  function postUser(req, res) => {
+    let user = new User(req.body.name, req.body.positions);
+    dao.postUser(user);
+    res.json({message: 'User added.', data: user});
+  }
+
+  function updateUser(req, res) => {
+    let user = new User(req.body.name, req.body.positions);
+    dao.updateUser(user, req.params.name);
+    res.json({message: 'User updated.', data: user});
+  }
+
+  function removeUser(req, res) => {
+    dao.removeUser(req.params.name);
+    res.json({message: 'User removed.', data: req.params.name});
+  }
+
     return {
-      getUsers: (req, res) => {
-        res.json(dao.getUsers());
-      },
-
-      getUser: (req, res) => {
-        res.json(dao.getUser(req.params.name));
-      },
-
-      postUser: (req, res) => {
-        let user = new User(req.body.name, req.body.positions);
-        dao.postUser(user);
-        res.json({message: 'User added.', data: user});
-      },
-
-      updateUser: (req, res) => {
-        let user = new User(req.body.name, req.body.positions);
-        dao.updateUser(user, req.params.name);
-        res.json({message: 'User updated.', data: user});
-      },
-
-      removeUser: (req, res) => {
-        dao.removeUser(req.params.name);
-        res.json({message: 'User removed.', data: req.params.name});
-      }
+      getUsers,
+      getUser,
+      postUser,
+      updateUser,
+      removeUser
     };
 
 };
